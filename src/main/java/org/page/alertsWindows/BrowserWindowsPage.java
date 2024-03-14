6d7b7af89f8007ca0fd80d2960af3842eddacf3a
Evgeniya <85844291+Larycolis@users.noreply.github.com>
2024-03-14 21:35:01 +0100

    implementation of page object pattern for test classes on Selenium iteration 2

--- src/main/java/org/page/alertsWindows/BrowserWindowsPage.java
+++ src/main/java/org/page/alertsWindows/BrowserWindowsPage.java
@@ -1,4 +1,93 @@
 package org.page.alertsWindows;
 
+import io.qameta.allure.Step;
+import org.junit.jupiter.api.Assertions;
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
 public class BrowserWindowsPage {
+    private final WebDriver driver;
+    private final WebDriverWait webDriverWait;
+
+    @FindBy(id = "tabButton")
+    private WebElement tabButton;
+
+    @FindBy(id = "windowButton")
+    private WebElement windowButton;
+
+    @FindBy(id = "messageWindowButton")
+    private WebElement messageWindowButton;
+
+    public BrowserWindowsPage(WebDriver driver) {
+        this.driver = driver;
+        webDriverWait = new WebDriverWait(this.driver, Duration.ofSeconds(5));
+        PageFactory.initElements(driver, this);
+    }
+
+    @Step("Performing a click using JavascriptExecutor")
+    public void clickJavascriptExecutor4(WebElement webElement) {
+        JavascriptExecutor executor = (JavascriptExecutor) driver;
+        executor.executeScript("arguments[0].click();", webElement);
+    }
+
+    @Step("Click the button, wait, switch and return new window")
+    public String clickToTabButtonAndSwitchToNewWindow() {
+        String originWindow = driver.getWindowHandle();
+        clickJavascriptExecutor4(tabButton);
+        webDriverWait.until(ExpectedConditions.numberOfWindowsToBe(2));
+        switchToNewWindow(originWindow);
+        return originWindow;
+    }
+
+    @Step("Click the button, wait, switch and return child window")
+    public String clickToWindowButtonAndSwitchToChildWindow() {
+        String originWindow = driver.getWindowHandle();
+        clickJavascriptExecutor4(windowButton);
+        webDriverWait.until(ExpectedConditions.numberOfWindowsToBe(2));
+        switchToNewWindow(originWindow);
+        return originWindow;
+    }
+
+    @Step("Click the button, wait, switch and return message window")
+    public String clickToMessageWindowButtonAndSwitchToChildWindow() {
+        String originWindow = driver.getWindowHandle();
+        clickJavascriptExecutor4(messageWindowButton);
+        webDriverWait.until(ExpectedConditions.numberOfWindowsToBe(2));
+        switchToNewWindow(originWindow);
+        return originWindow;
+    }
+
+    @Step("Checking contains the expected URL and the text in new window")
+    public void assertUrlAndValue() {
+        Assertions.assertTrue(driver.getCurrentUrl().contains("https://demoqa.com/sample"));
+        Assertions.assertEquals("This is a sample page", driver.findElement(By.id("sampleHeading")).getText());
+    }
+
+    @Step("Close new window and switch to the origin window")
+    public void closeChildWindowAndSwitchToOrigin(String originWindow) {
+        driver.close();
+        driver.switchTo().window(originWindow);
+    }
+
+    @Step("Checking that the values are not equal")
+    public void checkTheValuesAreNotEqual(String originWindow) {
+        Assertions.assertNotEquals(originWindow, driver.getWindowHandle());
+    }
+
+    private void switchToNewWindow(String originWindow) {
+        for (String windowHandle : driver.getWindowHandles()) {
+            if (!originWindow.equals(windowHandle)) {
+                driver.switchTo().window(windowHandle);
+                break;
+            }
+        }
+    }
 }
--- src/test/java/ui/alertsWindows/BrowserWindowsTests.java
+++ src/test/java/ui/alertsWindows/BrowserWindowsTests.java
@@ -1,89 +1,48 @@
 package ui.alertsWindows;
 
 import io.qameta.allure.Feature;
-import io.qameta.allure.Step;
 import io.qameta.allure.Story;
 import io.qameta.allure.junit5.AllureJunit5;
 import org.base.BaseTest;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.openqa.selenium.By;
-import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.page.alertsWindows.BrowserWindowsPage;
 
 @ExtendWith(AllureJunit5.class)
 @Feature("Brouser Windows Page")
 @Story("UI Tests")
 public class BrowserWindowsTests extends BaseTest {
-    private static final String SAMPLE_URL = "https://demoqa.com/sample";
-    private static final String SAMPLE_ID = "sampleHeading";
-    private static final String EXPECTED_TEXT = "This is a sample page";
-    private String originWindow;
+    private BrowserWindowsPage browserWindowsPage;
 
     @BeforeEach
     void setUp() {
+        browserWindowsPage = new BrowserWindowsPage(getDriver());
         getDriver().get("https://demoqa.com/browser-windows");
     }
 
     @Test
     @DisplayName("Click the button to see the tab")
     void clickNewTabButtonSwitchAndCheckNewTabTest() {
-        originWindow = clickToButtonAndSwitchToChildWindow("tabButton");
-        assertUrlAndValue();
-        closeChildWindowAndSwitchToOrigin(originWindow);
+        String originWindow = browserWindowsPage.clickToTabButtonAndSwitchToNewWindow();
+        browserWindowsPage.assertUrlAndValue();
+        browserWindowsPage.closeChildWindowAndSwitchToOrigin(originWindow);
     }
 
     @Test
     @DisplayName("Click the button to see the window")
     void clickNewWindowButtonSwitchAndCheckNewWindowTest() {
-        originWindow = clickToButtonAndSwitchToChildWindow("windowButton");
-        assertUrlAndValue();
-        closeChildWindowAndSwitchToOrigin(originWindow);
+        String originWindow = browserWindowsPage.clickToWindowButtonAndSwitchToChildWindow();
+        browserWindowsPage.assertUrlAndValue();
+        browserWindowsPage.closeChildWindowAndSwitchToOrigin(originWindow);
     }
 
     @Test
     @DisplayName("Click the button to see the window message")
     void clickNewWindowMessageButtonAndCheckNewWindowMessageTest() {
-        originWindow = clickToButtonAndSwitchToChildWindow("messageWindowButton");
-        checkTheValuesAreNotEqual();
-        closeChildWindowAndSwitchToOrigin(originWindow);
-    }
-
-    @Step("Click the button, wait, switch and return the child window")
-    private String clickToButtonAndSwitchToChildWindow(String tabButton) {
-        String originWindow = getDriver().getWindowHandle();
-        clickJavascriptExecutor(getDriver().findElement(By.id(tabButton)));
-        getWebDriverWait().until(ExpectedConditions.numberOfWindowsToBe(2));
-        switchToNewWindow(originWindow);
-        return originWindow;
-    }
-
-    @Step("Switch to the window")
-    private void switchToNewWindow(String originWindow) {
-        for (String windowHandle : getDriver().getWindowHandles()) {
-            if (!originWindow.equals(windowHandle)) {
-                getDriver().switchTo().window(windowHandle);
-                break;
-            }
-        }
-    }
-
-    @Step("Checking contains the expected URL and the text in new window")
-    private void assertUrlAndValue() {
-        Assertions.assertTrue(getDriver().getCurrentUrl().contains(SAMPLE_URL));
-        Assertions.assertEquals(EXPECTED_TEXT, getDriver().findElement(By.id(SAMPLE_ID)).getText());
-    }
-
-    @Step("Close new window and switch to the origin window")
-    private void closeChildWindowAndSwitchToOrigin(String originWindow) {
-        getDriver().close();
-        getDriver().switchTo().window(originWindow);
-    }
-
-    @Step("Checking that the values are not equal")
-    private void checkTheValuesAreNotEqual() {
-        Assertions.assertNotEquals(originWindow, getDriver().getWindowHandle());
+        String originWindow = browserWindowsPage.clickToMessageWindowButtonAndSwitchToChildWindow();
+        browserWindowsPage.checkTheValuesAreNotEqual(originWindow);
+        browserWindowsPage.closeChildWindowAndSwitchToOrigin(originWindow);
     }
 }
