4aec1ced0403666b90444c5eaa5da1ac8a3f6a4c
Evgeniya <85844291+Larycolis@users.noreply.github.com>
2024-03-16 00:16:53 +0100

    implementation of page object pattern for test classes on Selenium iteration 7

--- src/main/java/org/page/elements/DynamicPropertiesPage.java
+++ src/main/java/org/page/elements/DynamicPropertiesPage.java
@@ -1,4 +1,40 @@
 package org.page.elements;
 
+import io.qameta.allure.Step;
+import org.junit.jupiter.api.Assertions;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+
 public class DynamicPropertiesPage {
+    private final WebDriver driver;
+    private final WebDriverWait webDriverWait;
+
+    public DynamicPropertiesPage(WebDriver driver) {
+        this.driver = driver;
+        webDriverWait = new WebDriverWait(this.driver, Duration.ofSeconds(5));
+    }
+
+    @Step("Checking the text displayed on screen")
+    public void assertDisplayedTextWithRandomId() {
+        Assertions.assertEquals("This text has random Id", driver.findElement(By.cssSelector("div.row p")).getText());
+    }
+
+    @Step("Wait until the element becomes clickable")
+    public void checkClickableWillEnabledButton() {
+        webDriverWait.until(ExpectedConditions.elementToBeClickable(By.id("enableAfter")));
+    }
+
+    @Step("Wait until the element changes color")
+    public void checkColorChange() {
+        webDriverWait.until(ExpectedConditions.attributeContains(By.id("colorChange"), "class", "text-danger"));
+    }
+
+    @Step("Wait until the element becomes visible")
+    public void checkVisibility() {
+        webDriverWait.until(ExpectedConditions.visibilityOfElementLocated(By.id("visibleAfter")));
+    }
 }
--- src/test/java/ui/elements/DynamicPropertiesTests.java
+++ src/test/java/ui/elements/DynamicPropertiesTests.java
@@ -1,68 +1,47 @@
 package ui.elements;
 
 import io.qameta.allure.Feature;
-import io.qameta.allure.Step;
 import io.qameta.allure.Story;
 import io.qameta.allure.junit5.AllureJunit5;
 import org.base.BaseTest;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.openqa.selenium.By;
-import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.page.elements.DynamicPropertiesPage;
 
 @ExtendWith(AllureJunit5.class)
 @Feature("Dynamic Properties")
 @Story("UI Tests")
 public class DynamicPropertiesTests extends BaseTest {
+    private DynamicPropertiesPage dynamicPropertiesPage;
     @BeforeEach
     void setUp() {
+        dynamicPropertiesPage = new DynamicPropertiesPage(getDriver());
         getDriver().get("https://demoqa.com/dynamic-properties");
     }
 
     @Test
     @DisplayName("Checking that the text is equals")
     void checkTextIsDisplayed() {
-        checkEqualsExpAndActText("This text has random Id", By.cssSelector("div.row p"));
+        dynamicPropertiesPage.assertDisplayedTextWithRandomId();
     }
 
     @Test
     @DisplayName("Wait until the button becomes enable")
     void waitUntilEnabledAndClickTest() {
-        checkClickable();
+        dynamicPropertiesPage.checkClickableWillEnabledButton();
     }
 
     @Test
     @DisplayName("Wait until the button changes color")
     void waitUntilColorChangedAndClickTest() {
-        checkColor();
+        dynamicPropertiesPage.checkColorChange();
     }
 
     @Test
     @DisplayName("Wait until the button becomes visible")
     void waitUntilVisibleAndClickTest() {
-        checkVisibility();
-    }
-
-    @Step("Wait until the element becomes visible")
-    private void checkVisibility() {
-        getWebDriverWait().until(ExpectedConditions.visibilityOfElementLocated(By.id("visibleAfter")));
-    }
-
-    @Step("Checking that the text is equals")
-    private void checkEqualsExpAndActText(String expText, By selector) {
-        Assertions.assertEquals(expText, getDriver().findElement(selector).getText());
-    }
-
-    @Step("Wait until the element becomes clickable")
-    private void checkClickable() {
-        getWebDriverWait().until(ExpectedConditions.elementToBeClickable(getDriver().findElement(By.id("enableAfter"))));
-    }
-
-    @Step("Wait until the element changes color")
-    private void checkColor() {
-        getWebDriverWait().until(ExpectedConditions.attributeContains(getDriver().findElement(By.id("colorChange")), "class", "text-danger"));
+        dynamicPropertiesPage.checkVisibility();
     }
 }
