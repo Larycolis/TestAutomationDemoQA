65f5df3dd4dd305de98144f71e9082f17a98652b
Evgeniya <85844291+Larycolis@users.noreply.github.com>
2024-03-15 10:47:28 +0100

    implementation of page object pattern for test classes on Selenium iteration 5

--- src/main/java/org/page/elements/ButtonsPage.java
+++ src/main/java/org/page/elements/ButtonsPage.java
@@ -1,4 +1,64 @@
 package org.page.elements;
 
+import io.qameta.allure.Step;
+import org.junit.jupiter.api.Assertions;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
 public class ButtonsPage {
+    private final WebDriver driver;
+    private final Actions action;
+    @FindBy(id = "doubleClickBtn")
+    private WebElement doubleClickBtn;
+    @FindBy(id = "rightClickBtn")
+    private WebElement rightClickBtn;
+    @FindBy(id = "doubleClickMessage")
+    private WebElement doubleClickMessage;
+    @FindBy(id = "rightClickMessage")
+    private WebElement rightClickMessage;
+    @FindBy(id = "dynamicClickMessage")
+    private WebElement leftClickMessage;
+    @FindBy(css = "div.col-md-6 > div:nth-child(2) >  div:nth-child(4) button")
+    private WebElement leftClickBtn;
+
+    public ButtonsPage(WebDriver driver) {
+        this.driver = driver;
+        this.action = new Actions(this.driver);
+        PageFactory.initElements(driver, this);
+    }
+
+    @Step("Double click on the left mouse button")
+    public void doubleClick() {
+        action.doubleClick(doubleClickBtn).perform();
+    }
+
+    @Step("Right click on the left mouse button")
+    public void rightClick() {
+        action.contextClick(rightClickBtn).perform();
+    }
+
+    @Step("Assert text appearing after double click")
+    public void assertTextAfterDoubleClick() {
+        Assertions.assertEquals("You have done a double click", doubleClickMessage.getText());
+    }
+
+    @Step("Assert text appearing after right click")
+    public void assertTextAfterRightClick() {
+        Assertions.assertEquals("You have done a right click", rightClickMessage.getText());
+    }
+
+    @Step("Assert text appearing after left click")
+    public void clickAndAssertTextAfterLeftClick() {
+        clickJavascriptExecutor6(leftClickBtn);
+        Assertions.assertEquals("You have done a dynamic click", leftClickMessage.getText());
+    }
+
+    private void clickJavascriptExecutor6(WebElement webElement) {
+        JavascriptExecutor executor = (JavascriptExecutor) driver;
+        executor.executeScript("arguments[0].click();", webElement);
+    }
 }
--- src/test/java/ui/elements/ButtonsTests.java
+++ src/test/java/ui/elements/ButtonsTests.java
@@ -1,62 +1,43 @@
 package ui.elements;
 
 import io.qameta.allure.Feature;
-import io.qameta.allure.Step;
 import io.qameta.allure.Story;
 import io.qameta.allure.junit5.AllureJunit5;
 import org.base.BaseTest;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.openqa.selenium.By;
-import org.openqa.selenium.interactions.Actions;
+import org.page.elements.ButtonsPage;
 
 @ExtendWith(AllureJunit5.class)
 @Feature("Buttons")
 @Story("UI Tests")
 public class ButtonsTests extends BaseTest {
-    private final Actions action = new Actions(getDriver());
-
+    private ButtonsPage buttonsPage;
     @BeforeEach
     void setUp() {
+        buttonsPage = new ButtonsPage(getDriver());
         getDriver().get("https://demoqa.com/buttons");
     }
 
     @Test
     @DisplayName("Check double left button click action")
     void clickButtonDoubleClickMeAndCheckSelectedTest() {
-        doubleClick(By.id("doubleClickBtn"));
-        checkEqualsExpAndActText("You have done a double click", By.id("doubleClickMessage"));
+        buttonsPage.doubleClick();
+        buttonsPage.assertTextAfterDoubleClick();
     }
 
     @Test
     @DisplayName("Check right button click action")
     void clickButtonRightClickMeAndCheckSelectedTest() {
-        rightClick(By.id("rightClickBtn"));
-        checkEqualsExpAndActText("You have done a right click", By.id("rightClickMessage"));
+        buttonsPage.rightClick();
+        buttonsPage.assertTextAfterRightClick();
     }
 
     @Test
     @DisplayName("Check left button click action")
     void clickButtonClickMeAndCheckSelectedTest() {
-        clickJavascriptExecutor(getDriver().findElement(By.cssSelector("div.col-md-6 > div:nth-child(2) >  div:nth-child(4) button")));
-        checkEqualsExpAndActText("You have done a dynamic click", By.id("dynamicClickMessage"));
-    }
-
-    @Step("Double click on the left mouse button")
-    private void doubleClick(By selector) {
-        action.doubleClick(getDriver().findElement(selector)).perform();
-    }
-
-    @Step("Right click on the left mouse button")
-    private void rightClick(By selector) {
-        action.contextClick(getDriver().findElement(selector)).perform();
-    }
-
-    @Step("Checking that the text is equals")
-    private void checkEqualsExpAndActText(String expText, By selector) {
-        Assertions.assertEquals(expText, getDriver().findElement(selector).getText());
+        buttonsPage.clickAndAssertTextAfterLeftClick();
     }
 }
